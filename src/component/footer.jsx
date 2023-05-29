b98b208ca93d99ddd4bdb47f00cf82d1d3dbc2f9
arryadali <arryadali@gmail.com>
2023-05-29 23:54:23 +0700

    percobaan 2

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return(
         <div className="container">
             <div className="bg-[#010C80] h-[78px] text-[20px] font-medium text-center mt-[4%]">
-                &copy; 2023. Kita Kontol.
+                &copy; 2023. Kita Sehat.
             </div>
         </div>
     )
